4ec14ddf67da59f5864e40b19195a4f2907611ce
emon1995 <emonhossain995@gmail.com>
2023-04-29 11:22:38 +0600

    cors middlerware added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const categories = require("./data/categories.json");
 const news = require("./data/news.json");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
